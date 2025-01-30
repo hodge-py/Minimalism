286c0e306fd79fd7086d74fa19ffcdfe22074840
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-01-30 16:36:37 -0700

    c

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
+    var storeFile = 1
 
-    $("#arrowNext").click(function(){
-        $("#container1").fadeOut("slow", function(){
-            $("#container2").fadeIn();
-        });
+    $(".arrowNext").click(function(event){
+
+        if($(this).text() == 'About') {
+            $(`#container${storeFile}`).fadeOut("slow", function () {
+                $("#container2").fadeIn("slow");
+                storeFile = 2
+            });
+        }
+        else if($(this).text() == "Home"){
+            $(`#container${storeFile}`).fadeOut("slow", function () {
+                $("#container1").fadeIn("slow");
+                storeFile = 1
+            });
+        }
+        else if($(this).text() == "Blog"){
+            $(`#container${storeFile}`).fadeOut("slow", function () {
+                $("#container3").fadeIn("slow");
+                storeFile = 3
+            });
+        }
+
+
+
     })
 
 });
